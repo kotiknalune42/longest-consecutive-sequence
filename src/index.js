d5a99583931f98914ab5441d5d3b5197c5c00a9c
Катрин Райлян <k.shalamowa@gmail.com>
2018-09-27 13:37:37 +0300

    Passed all tests

--- src/index.js
+++ src/index.js
@@ -1,34 +1,17 @@
-module.exports = function longestConsecutiveLength(num) {
-  // if array is empty, return 0
-    if (num.length == 0) {
-        return 0;
-    }
- 
-    var set = [];
-    var max = 1;
- 
-    for (var i=0; i<num.length; i++)
-        set.push(num[i]);
- 
-    for (var i=0; i<num.length; i++) {
-        var left = num[i] - 1;
-        var right = num[i] + 1;
-        var count = 1;
- 
-        while (set.indexOf(left) != -1) {
-            count++;
-            set.splice(set.indexOf(left), 1);
-            left--;
-        }
- 
-        while (set.indexOf(right) != -1) {
-            count++;
-            set.splice(set.indexOf(right), 1);
-            right++;
-        }
- 
-        max = Math.max(count, max);
-    }
- 
-    return max;
+module.exports = function longestConsecutiveLength(array) {
+    let len = array.length, count = 1, sequence = 0;
+    if(len === 0) return 0;
+    if(len === 1) return 1;
+
+  array.sort(function(a,b){return a - b;});
+  for(number in array) {
+    let x = array[number], y = array[number - 1];
+     if(x - y === 0) continue;
+     if(x - y === 1) count++;
+     if(x - y > 1){
+        sequence = (count > sequence)? count : sequence;
+        count=1;
+  }
+}
+    return sequence; 
 }
