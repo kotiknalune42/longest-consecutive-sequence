71ef00f12d7b1c7c99b679c39accda2080692526
Катрин Райлян <k.shalamowa@gmail.com>
2018-09-27 11:59:14 +0300

    test2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 module.exports = function longestConsecutiveLength(array) {
- let set = {}, longest = 0;
- for (number in array) {
- 	set[number] = true;
- }
- for(number in set) {
- 	if(set[number] - 1) {
- 		let current = number, count = 1;
- 		while (set[curr + 1]){
- 			count++;
- 			current++;
- 		}
- 	longest = Math.max(longest.count);
- 	}
- }
- return longest;
+  // if array is empty, return 0
+    if (num.length == 0) {
+        return 0;
+    }
+ 
+    var set = [];
+    var max = 1;
+ 
+    for (var i=0; i<num.length; i++)
+        set.push(num[i]);
+ 
+    for (var i=0; i<num.length; i++) {
+        var left = num[i] - 1;
+        var right = num[i] + 1;
+        var count = 1;
+ 
+        while (set.indexOf(left) != -1) {
+            count++;
+            set.splice(set.indexOf(left), 1);
+            left--;
+        }
+ 
+        while (set.indexOf(right) != -1) {
+            count++;
+            set.splice(set.indexOf(right), 1);
+            right++;
+        }
+ 
+        max = Math.max(count, max);
+    }
+ 
+    return max;
 }
