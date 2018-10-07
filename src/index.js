b6357a954081fe5b8f21a0850233b7df60b30cf3
Катрин Райлян <k.shalamowa@gmail.com>
2018-10-07 20:25:15 +0300

    refactor: refactor sort

--- src/index.js
+++ src/index.js
@@ -5,15 +5,13 @@ module.exports = function longestConsecutiveLength(array) {
     if (len === 0) return 0;
     if (len === 1) return 1;
 
-    array.sort(function(a, b) {
-        return a - b;
-    });
+    array.sort(function(a, b) => a - b);
 
     for (number in array) {
         let x = array[number],
             y = array[number - 1];
         // console.log("number:" + number + "x " + x + "y " + y)
-        if (x - y === 'undefined') continue;
+        if (x - y === 0) continue; // first loop gives undefinded, which technically is a bug but undefined == 0 so.... 
         if (x - y === 1) count++;
         if (x - y > 1) {
             sequenceLength = (count > sequenceLength) ? count : sequenceLength;
